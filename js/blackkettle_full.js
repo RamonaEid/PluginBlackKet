f5ba16396b14e4b378361963f24b7c140f9d7877
RamonaEid <eid@intermark.com>
2016-08-14 10:57:20 -0600

    Known issue with jQueryUI Tabs NOT Compatible with Custmizer  Version 0.0.4
    Customizer is Incompatible with jQuery UI Tabs.  Known bug.  Used
    JavaScript from https://bugs.jqueryui.com/ticket/8637  Version 0.0.4

--- js/blackkettle_full.js
+++ js/blackkettle_full.js
@@ -7,28 +7,24 @@ jQuery(document).ready(function (i) {
         Supporter: 'Tara Dunn'
     }
 
+    // Customizer is Incompatible with jQuery UI Tabs.
+    //https://core.trac.wordpress.org/ticket/23225 (punted from 4.6 to Future Release)
+    //https://bugs.jqueryui.com/ticket/8637
+    jQuery.fn.__tabs = jQuery.fn.tabs;
+    jQuery.fn.tabs = function (a, b, c, d, e, f) {
+        var base = window.location.href.replace(/#.*jQuery/, '');
+        jQuery('ul>li>a[href^="#"]', this).each(function () {
+            var href = jQuery(this).attr('href');
+            jQuery(this).attr('href', base + href);
+        });
+        jQuery(this).__tabs(a, b, c, d, e, f);
+    };
+
+
+
     //jQuery(function() {
     jQuery("#tabs").tabs();
     //});
-
-    // Customizer is Incompatible with jQuery UI Tabs.
-    //https://core.trac.wordpress.org/ticket/23225 (punted from 4.6 to Future Release)
-    //https://www.tjvantoll.com/2013/02/17/using-jquery-ui-tabs-with-the-base-tag/ (use #3)
-    var makeTabs = function (selector) {
-        jQuery(selector)
-            .find("ul a").each(function() {
-                var href = jQuery(this).attr("href"),
-                    newHref = window.location.protocol + "//" + window.location.hostname +
-                        window.location.pathname + href;
-
-                if (href.indexOf("#") == 0) {
-                    jQuery(this).attr("href", newHref);
-                }
-            });
-        jQuery(selector).tabs();
-    };
-
-    makeTabs("#tabs");
 
 
 });
