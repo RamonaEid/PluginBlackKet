7db96e94fb3585260128ce0d89ca2696f09c1e56
RamonaEid <eid@intermark.com>
2016-08-14 10:10:38 -0600

    Known bug jQueryUI Tabs NOT Compatible with Customizer.  Version 0.0.3
    Customizer is Incompatible with jQueryUI Tabs.  Known bug trac ticket
    #23225.  Used JavaScript as workaround for now.  Future Release.

--- js/blackkettle_full.js
+++ js/blackkettle_full.js
@@ -8,7 +8,27 @@ jQuery(document).ready(function (i) {
     }
 
     //jQuery(function() {
-        jQuery("#tabs").tabs();
+    jQuery("#tabs").tabs();
     //});
+
+    // Customizer is Incompatible with jQuery UI Tabs.
+    //https://core.trac.wordpress.org/ticket/23225 (punted from 4.6 to Future Release)
+    //https://www.tjvantoll.com/2013/02/17/using-jquery-ui-tabs-with-the-base-tag/ (use #3)
+    var makeTabs = function (selector) {
+        jQuery(selector)
+            .find("ul a").each(function() {
+                var href = jQuery(this).attr("href"),
+                    newHref = window.location.protocol + "//" + window.location.hostname +
+                        window.location.pathname + href;
+
+                if (href.indexOf("#") == 0) {
+                    jQuery(this).attr("href", newHref);
+                }
+            });
+        jQuery(selector).tabs();
+    };
+
+    makeTabs("#tabs");
+
 
 });
